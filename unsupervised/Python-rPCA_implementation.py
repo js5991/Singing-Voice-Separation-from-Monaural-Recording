9ede31607e68d73b1f2f4c167955aee40f07ced6
gaolingshan <lynngao823@gmail.com>
2017-12-01 20:42:26 -0500

    Update Python-rPCA_implementation.py

--- unsupervised/Python-rPCA_implementation.py
+++ unsupervised/Python-rPCA_implementation.py
@@ -24,8 +24,8 @@ from bss_eval import bss_eval_sources
 
 # Data_Path
 
-data_path = '../../data/MIR-1K_for_MIREX/Wavfile/'
-#data_path = '/scratch/lg2755/valid_1'
+#data_path = '../../data/MIR-1K_for_MIREX/Wavfile/'
+data_path = '/scratch/lg2755/valid_1'
 #data_path = '/scratch/js5991/opt/valid'
 data = process_data.Data(data_path)
 #valid_data = header.Data(valid_data_path)
